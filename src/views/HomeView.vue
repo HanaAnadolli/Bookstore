ec9494a926de95efe52912b623617a7f5121feb9
HanaAnadolli <hanadolli02@gmail.com>
2023-01-26 23:07:55 +0100

    Carousel added to project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home">
+    <Carousel
+      :navigation="true"
+      :pagination="true"
+      :startAutoPlay="false"
+      :timeout="5000"
+      class="carousel"
+      v-slot="{ currentSlide }"
+    >
+      <Slide v-for="(slide, index) in carouselSlides" :key="index">
+        <div v-show="currentSlide === index + 1" class="slide-info">
+          <img :src="require(`../assets/${slide}.jpg`)" alt="" />
+        </div>
+      </Slide>
+    </Carousel>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Carousel from '@/components/Carousel.vue';
+import Slide from '@/components/Slide.vue';
+
+export default {
+  name: 'HomeView',
+  components:{
+    Carousel,
+    Slide
+  },
+  setup() {
+    const carouselSlides = ["photo1", "photo2", "photo3", "photo4", "photo5"];
+
+    return {carouselSlides}
+  }
+}
+
+</script>
+
+<style lang="scss" scoped>
+.carousel {
+  max-height: 100vh;
+  height: 80vh;
+  .slide-info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    max-height: 100%;
+    height: 100%;
+    img {
+      min-width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
+</style>
