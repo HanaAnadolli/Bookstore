ce302bca8700109301be28ba74170d5d27723f8f
SaraBajraktari02 <104868790+SaraBajraktari02@users.noreply.github.com>
2023-03-07 05:15:59 -0800

    api for contact form added

--- backend/index.js
+++ backend/index.js
@@ -15,6 +15,8 @@ mongoose
 
 
   const bookAPI = require('./routes/book.route')
+  const contactAPI = require('./routes/contact.route')
+  
   const app = express()
   app.use(bodyParser.json())
   app.use(
@@ -26,12 +28,26 @@ mongoose
   
   // API
   app.use('/api', bookAPI)
+  app.use('/apiC', contactAPI)
   
   // Create port
   const port = process.env.PORT || 4000
   const server = app.listen(port, () => {
     console.log('Connected to port ' + port)
   })
+  
+  // Find 404
+  app.use((req, res, next) => {
+    next(createError(404))
+  })
+  
+  // error handler
+  app.use(function (err, req, res, next) {
+    console.error(err.message)
+    if (!err.statusCode) err.statusCode = 500
+    res.status(err.statusCode).send(err.message)
+  })
+  
   
   // Find 404
   app.use((req, res, next) => {
