5fdc99cd6da6f1976abb46f05a2138a7310fc0f6
HanaAnadolli <86631574+HanaAnadolli@users.noreply.github.com>
2023-03-12 04:19:16 +0100

    main with some changes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import store from '@/store'
+import './firebase';
+import 'bootstrap/dist/css/bootstrap.css'
+import '@fortawesome/fontawesome-free/js/all'
+
 
 const app = createApp(App);
 
 app.use(router);
 
+app.use(store);
+
 app.mount('#app')
-
-
