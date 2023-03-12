6209fc1abfdcf2070669c9e65432b4dc36f37314
HanaAnadolli <86631574+HanaAnadolli@users.noreply.github.com>
2023-03-12 04:16:03 +0100

    Delete firebase directory

--- firebase/user/loginUser.js
+++
@@ -1,9 +0,0 @@
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-
-async function loginUser(payload) {
-    const {email, password} = payload;
-    const auth = getAuth();
-    return await signInWithEmailAndPassword(auth, email, password);
-}
-
-export default loginUser;
