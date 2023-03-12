dffc1ae3b7151e2c56d484c130f07f77e61dc2a3
HanaAnadolli <86631574+HanaAnadolli@users.noreply.github.com>
2023-03-12 04:24:49 +0100

    index added

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
-import AboutUs from '../views/AboutUs.vue'
+import ShopView from '../views/ShopView.vue'
+import LogIn from '../views/User/Login.vue'
+import SignUp from '../views/User/Signup.vue'
+import Dashboard from '../views/Dashboard.vue'
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 
 const routes = [
@@ -11,30 +14,15 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/info',
-    name: 'info',
-    component: AboutUs
-
+    path: '/shop',
+    name: 'Shop',
+    component: ShopView
   }, 
-
-  {
-    path: '/listFavorite',
-    name: 'listFavorite',
-    component: () => import('../components/ListFavoriteBookComponent'),
-  },
- 
   {
     path: '/create',
     name: 'create',
     component: () => import('../components/CreateComponent'),
   },
-
-
   {
     path: '/list',
     name: 'list',
@@ -55,6 +43,24 @@ const routes = [
     name: 'createContacts',
     component: () => import('../components/CreateContactComponent'),
   },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    meta: {
+      authRequires: true
+    }
+  },
+  {
+    path: '/signup',
+    name: 'signUp',
+    component: SignUp
+  },
+  {
+    path: '/login',
+    name: 'LogIn',
+    component: LogIn
+  }
 ]
 
 /* komponentet perbehen prej 3 pjeseve ato jane template script dhe style */
@@ -63,5 +69,19 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+const guestRouteNames = ['login', 'register'];
+
+router.beforeEach((to, from, next) => {
+  const auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if ((guestRouteNames.includes(to.name)) && user) {
+      next({ name: 'home' });
+    }
+    else{
+      next();
+    }
+  })
+});
 
 export default router
