d7283b05d0d6525361961bccc645defd739f64dd
SaraBajraktari02 <104868790+SaraBajraktari02@users.noreply.github.com>
2023-03-12 08:45:33 -0700

    Add files via upload

---
+++ src/components/Footer.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="copyright" style="background-color:#eee6d6;">
+     <div class="container">
+         <div class="row">
+             <div class="col-lg-6 col-md-12 col-sm-12">
+                 <ul class="list-unstyled li-space-lg p-small">
+                     <li><a >Article Details</a></li>
+                     <li><a >Terms & Conditions</a></li>
+                     <li><a >Privacy Policy</a></li>
+                 </ul>
+             </div> 
+             <div class="col-lg-3 col-md-12 col-sm-12">
+                 <p class="p-small statement">Copyright © <a >Hana Mirjeta Sara </a></p>
+             </div> 
+             <div class="col-lg-3 col-md-12 col-sm-12">
+           
+             </div> 
+         </div> 
+     </div> 
+   </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+.copyright {
+	padding-top: 1.5rem;
+	border-top: 1px solid #d0dae2;
+	text-align: center;
+}
+
+.copyright a {
+	text-decoration: none;
+}
+
+.copyright .list-unstyled,
+.copyright .statement {
+	opacity: 0.7;
+}
+  .container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 0 10px;
+  }
+</style>
